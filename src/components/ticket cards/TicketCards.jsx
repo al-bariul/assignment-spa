3863446e0b66bfbe598345e2bc22ec4d551968cc
al bariul <albariul475@gmail.com>
2026-01-06 18:40:59 +0600

    open and in-progress logic completed

--- src/components/ticket cards/TicketCards.jsx
+++ src/components/ticket cards/TicketCards.jsx
@@ -1,35 +1,56 @@
 import React, { use } from "react";
+// import { useState } from "react";
 
 const TicketCards = ({ ticketPromise }) => {
   const ticketData = use(ticketPromise);
   console.log(ticketData);
+  //   const [color, setColor] = useState(true);
   return (
     <div className="bg-yellow-500 w-[1000px] h-auto mx-auto">
       <div className="title">
         <h1>Customer Tickets</h1>
       </div>
       <div className="grid grid-cols-2 gap-y-5">
-        {ticketData.map(() => {
+        {ticketData.map((tdata) => {
+          const {
+            title,
+            description,
+            customer,
+            priority,
+            status,
+            createdAt,
+            id,
+          } = tdata;
+
+          const ticketStatus = status;
+          //   console.log(ticketStatus);
+          //   console.log(toUpperCase(status));
+          const ticketStatusToLowerCase = ticketStatus.toLowerCase();
           return (
             <div>
               <div className="card w-96 bg-white card-xs shadow-sm">
                 <div className="card-body">
                   <div className="text-header flex justify-between items-center border-2">
-                    <h2 className="card-title">Xsmall Card</h2>
-                    <span className="border-2">Open</span>
+                    <h2 className="card-title">{title}</h2>
+                    <span
+                      className={`border-2 ${
+                        ticketStatusToLowerCase === "open"
+                          ? "text-[#0B5E06] bg-[#B9F8CF] px-3 py-0.5 font-bold"
+                          : "text-amber-500"
+                      }`}
+                    >
+                      {status}
+                    </span>
                   </div>
-                  <p>
-                    A card component has a figure, a body part, and inside body
-                    there are title and actions parts
-                  </p>
+                  <p>{description}</p>
                   <div className="flex justify-between">
                     <div className="div flex border-2">
-                      <p className="mr-2">#1003</p>
-                      <p>HIGH PRIORITY</p>
+                      <p className="mr-2">#{id}</p>
+                      <p>{priority}</p>
                     </div>
                     <div className="div flex border-2">
-                      <p className="mr-2">John Smith</p>
-                      <p>1/15/2024</p>
+                      <p className="mr-2">{customer}</p>
+                      <p>{createdAt}</p>
                     </div>
                   </div>
                 </div>
