39aeb444c079527bb9a69d438d76a81177dcae91
al bariul <albariul475@gmail.com>
2026-01-06 15:30:44 +0600

    showing cards data

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import "./App.css";
 import Banner from "./components/banner/Banner";
 import Footer from "./components/footer/Footer";
@@ -5,13 +6,24 @@ import Navbar from "./components/navigationBar/Navbar";
 import TicketCards from "./components/ticket cards/TicketCards";
 import TicketStatus from "./components/ticket status/TicketStatus";
 
+const ticketData = async () => {
+  const response = await fetch("/public/ticket.json");
+  return response.json();
+};
+
 function App() {
+  const ticketPromise = ticketData();
+
   return (
     <>
       <Navbar></Navbar>
       <Banner></Banner>
       <div className="border-2 flex w-[1200px] mx-auto gap-x-16 mt-16">
-        <TicketCards></TicketCards>
+        <Suspense
+          fallback={<span className="loading loading-ring loading-xl"></span>}
+        >
+          <TicketCards ticketPromise={ticketPromise}></TicketCards>
+        </Suspense>
         <TicketStatus></TicketStatus>
       </div>
       <Footer></Footer>
--- src/components/ticket cards/TicketCards.jsx
+++ src/components/ticket cards/TicketCards.jsx
@@ -1,34 +1,42 @@
-import React from "react";
+import React, { use } from "react";
 
-const TicketCards = () => {
+const TicketCards = ({ ticketPromise }) => {
+  const ticketData = use(ticketPromise);
+  console.log(ticketData);
   return (
-    <div className="bg-yellow-500 w-[1000px] h-[700px] mx-auto">
+    <div className="bg-yellow-500 w-[1000px] h-auto mx-auto">
       <div className="title">
         <h1>Customer Tickets</h1>
       </div>
-      <div>
-        <div className="card w-96 bg-white card-xs shadow-sm">
-          <div className="card-body">
-            <div className="text-header flex justify-between items-center border-2">
-              <h2 className="card-title">Xsmall Card</h2>
-              <span className="border-2">Open</span>
-            </div>
-            <p>
-              A card component has a figure, a body part, and inside body there
-              are title and actions parts
-            </p>
-            <div className="flex justify-between">
-              <div className="div flex border-2">
-                <p className="mr-2">#1003</p>
-                <p>HIGH PRIORITY</p>
+      <div className="grid grid-cols-2 gap-y-5">
+        {ticketData.map(() => {
+          return (
+            <div>
+              <div className="card w-96 bg-white card-xs shadow-sm">
+                <div className="card-body">
+                  <div className="text-header flex justify-between items-center border-2">
+                    <h2 className="card-title">Xsmall Card</h2>
+                    <span className="border-2">Open</span>
+                  </div>
+                  <p>
+                    A card component has a figure, a body part, and inside body
+                    there are title and actions parts
+                  </p>
+                  <div className="flex justify-between">
+                    <div className="div flex border-2">
+                      <p className="mr-2">#1003</p>
+                      <p>HIGH PRIORITY</p>
+                    </div>
+                    <div className="div flex border-2">
+                      <p className="mr-2">John Smith</p>
+                      <p>1/15/2024</p>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="div flex border-2">
-                <p className="mr-2">John Smith</p>
-                <p>1/15/2024</p>
-              </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
